a34293228824c5212998a678f7c991db02983a82
Anonyo Hossain <anonyo.hossain@koolicar.com>
2018-10-02 13:02:42 -0400

    first commit

---
+++ app/assets/javascripts/components/_body.js.jsx
@@ -0,0 +1,19 @@
+class Body extends React.Component {
+constructor(props) {
+    super(props);
+    this.state = {
+      fruits: []
+    };
+  }
+componentDidMount(){
+    fetch('/api/v1/fruits.json')
+      .then((response) => {return response.json()})
+      .then((data) => {this.setState({ fruits: data }) });
+  }
+render(){
+    return(
+      <div>
+      </div>
+    )
+  }
+}
